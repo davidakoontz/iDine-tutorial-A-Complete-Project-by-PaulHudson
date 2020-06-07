6a14659cb7bf8ae1e9b3b62b58474c90b387c98c
davidakoontz <david@koontz.name>
2020-06-06 20:21:37 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by David on 6/6/20.
+//  Copyright © 2020 Life Works IQ Consulting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
