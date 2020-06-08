2bca240809c1f302b2c3da337ae9320c1d865fac
davidakoontz <david@koontz.name>
2020-06-07 19:30:30 -0500

    Finished project.  Added Checkout view.   Still haven't cleared out our memory of ordered items.  But we do have the ability to edit & delete the items on the order.
    Paul made some suggestions for enhancing

--- README.md
+++ README.md
@@ -6,4 +6,16 @@ Videos on YouTube.
 
 Started June 6, 2020 - David
 
+June 7
+Observable objects, environment objects, and @Published
+ Adding items to an order with @EnvironmentObject
+Adding TabView and tabItem()
+Bindings and forms
+Two-way bindings in SwiftUI
+Formatting interpolated strings in SwiftUI
+Presenting an alert
+Adding swipe to delete and EditButton
+
+
+
 
---
+++ iDine/AppView.swift
@@ -0,0 +1,35 @@
+//
+//  AppView.swift
+//  iDine
+//
+//  Created by David on 6/7/20.
+//  Copyright © 2020 Life Works IQ Consulting. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Menu")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Order")
+            }
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static let order = Order()
+    
+    static var previews: some View {
+        AppView().environmentObject(order)
+    }
+}
